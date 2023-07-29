10a2089b1485b2cd9732f664694bd7b351cbc039
numi3 <numiyemin1@gmail.com>
2023-07-29 17:52:41 +0300

    -

---
+++ auctions/forms.py
@@ -0,0 +1,74 @@
+from django.utils import timezone
+from django import forms
+from .models import Auction, Bid, Comment, Watchlist
+from django.core.validators import MinValueValidator
+from decimal import Decimal
+
+
+class AuctionForm(forms.ModelForm):
+    
+    class Meta:
+        model = Auction
+        fields = [
+            "title",
+            "description",
+            "image_url",
+            "category",
+            "end_time",
+            "starting_bid"
+        ]
+        
+        widgets = {
+            "end_time": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+            "starting_bid": forms.NumberInput(attrs={"min": "0", "value": "5.00", "step": 1})
+        }
+        
+        
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+        if len(title) < 6:
+            raise forms.ValidationError("Title must be at least 6 characters long.")
+        return title
+    
+    
+    def clean_end_time(self):
+        if end_time := self.cleaned_data.get("end_time"):
+            current_time = timezone.now()
+            time_difference = end_time - current_time
+            if time_difference.total_seconds() <= 3600:
+                raise forms.ValidationError("The end time must be at least 1 hour from the current time.")
+            elif time_difference.days > 180:
+                raise forms.ValidationError("The end time cannot be more than 180 days from the current time.")
+        return end_time
+
+class BidForm(forms.ModelForm):
+    class Meta:
+        model = Bid
+        fields = ["amount"]
+        
+    def clean_amount(self):
+        
+        amount = self.cleaned_data.get("amount")        
+        try:
+            current_auction = Auction.objects.get(id=self.initial.get("auction_id"))
+            starting_bid = Decimal(current_auction.starting_bid)
+            
+            # check if higher than starting bid:
+            if amount < starting_bid:
+                raise forms.ValidationError("Cannot bid less than the starting bid.")
+
+            # check if higher than current bid if exists.
+            if current_auction.current_bid is not None:
+                current_bid = Decimal(current_auction.current_bid.amount)
+                if amount <= current_bid:
+                    raise forms.ValidationError("Cannot bid less than the current bid.")
+            return amount
+        except Auction.DoesNotExist:
+            raise forms.ValidationError("Auction does not exist.")
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["content"]
+        
+    
--- auctions/views.py
+++ auctions/views.py
@@ -3,17 +3,41 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
-from .models import User
+from .models import *
+from .forms import *
+from .utils import *
 
 
+def all(request):
+    # Load & render all auctions
+    return render(request, "Auctions/index.html", {
+        "auctions": Auction.objects.all().order_by("-id"),
+        "empty_message": "There are no listings."
+    })
+
+    
 def index(request):
-    return render(request, "auctions/index.html")
+    # Load & render active auctions
+    return render(request, "auctions/index.html", {
+        "auctions": Auction.objects.filter(is_active=True).order_by("-id"),
+        "empty_message": "There are no currently active listings."
+    })
+
+
+def watchlist(request):
+    # Load & render all auctions that have been added to the user's watchlist.
+    return render(request, "auctions/index.html", {
+        "auctions": Auction.objects.filter(watchlist_auction__user=request.user),
+        "empty_message": "You haven't added any listings to your watchlist."
+    })
 
 
 def login_view(request):
     if request.method == "POST":
-
+        
         # Attempt to sign user in
         username = request.POST["username"]
         password = request.POST["password"]
@@ -22,7 +46,7 @@ def login_view(request):
         # Check if authentication successful
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse("index"))
+            return HttpResponseRedirect(reverse("auctions:index"))
         else:
             return render(request, "auctions/login.html", {
                 "message": "Invalid username and/or password."
@@ -33,7 +57,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse("index"))
+    return HttpResponseRedirect(reverse("auctions:index"))
 
 
 def register(request):
@@ -58,6 +82,132 @@ def register(request):
                 "message": "Username already taken."
             })
         login(request, user)
-        return HttpResponseRedirect(reverse("index"))
+        return HttpResponseRedirect(reverse("auctions:index"))
     else:
         return render(request, "auctions/register.html")
+
+
+@login_required(login_url="/login/")
+def new(request):
+    if request.method == "POST":
+        form = AuctionForm(request.POST)
+        if form.is_valid():
+            auction = form.save(commit=False)
+            auction.created_by = request.user
+            auction.save()
+            return HttpResponseRedirect(reverse("auctions:index"))
+
+    return render(request, "auctions/new.html", {
+        "form": AuctionForm()
+    })
+
+
+def no_listing(request):  # in case id was not provided:
+    return HttpResponseRedirect(reverse("auctions:index"))
+
+
+def listing(request, id):
+    auction = Auction.objects.get(id=id)
+    if request.method == "POST":
+        # adds comment
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.auction = auction
+            comment.commenter = request.user
+            comment.save()
+        return HttpResponseRedirect(reverse("auctions:listing", args=[id]))
+
+    return render(request, "auctions/listing.html", {
+        "auction": Auction.objects.get(id=id),
+        "is_in_watchlist": is_in_watchlist(request.user, auction),
+        "comments": Comment.objects.filter(auction=auction).order_by("-date"),
+        "comment_area": CommentForm()
+
+    })
+
+
+@login_required(login_url="/login/")
+def bid(request, id):
+    if request.method == "POST":
+        form = BidForm(request.POST, initial={"auction_id": id})
+        if form.is_valid():
+            print("posted")
+            bid = form.save(commit=False)
+            auction = Auction.objects.get(id=id)
+            bid.auction_id = auction
+            bid.bidder = request.user
+            auction.current_bid = bid
+            bid.save()
+            auction.save()
+            return HttpResponseRedirect(reverse("auctions:listing", args=[id]))
+    if (current_listing := Auction.objects.get(id=id)) is not None:
+        return render(request, "auctions/bid.html", {
+            "auction": current_listing,
+            "bid_form": BidForm
+        })
+
+
+def is_in_watchlist(user, auction):
+    return Watchlist.objects.filter(user=user, auction=auction).exists()
+
+
+@login_required(login_url="/login/")
+def watchlist_add(request, id):
+    auction = Auction.objects.get(id=id)
+    if not is_in_watchlist(request.user, auction):
+        watchlist = Watchlist(user=request.user, auction=auction)
+        watchlist.save()
+    return HttpResponseRedirect(reverse("auctions:listing", args=[id]))
+
+
+@login_required(login_url="/login/")
+def watchlist_remove(request, id):
+    auction = Auction.objects.get(id=id)
+    if is_in_watchlist(request.user, auction):
+        watchlist = Watchlist.objects.filter(
+            user=request.user, auction=auction)
+        watchlist.delete()
+    return HttpResponseRedirect(reverse("auctions:listing", args=[id]))
+
+
+def edit(request, id):
+    if request.method == "POST":
+        pass
+    
+    return render(request, "auctions/edit.html", {
+        "form": AuctionForm
+    })
+    
+
+@login_required(login_url="/login/")
+def close(request, id):
+    if auction := Auction.objects.get(id=id):
+        if request.user == auction.created_by and auction.is_active:
+            if auction.current_bid is not None:
+                auction.winner = auction.current_bid.bidder
+            auction.end_time = timezone.now()
+            auction.is_active = False
+            auction.save()
+            
+    return HttpResponseRedirect(reverse("auctions:listing", args=[id]))
+    
+def categories(request):
+    active_auctions = Auction.objects.filter(is_active=True).order_by("-id")
+    active_auctions = Auction.objects.filter()
+    categories = {}
+    for auction in active_auctions:
+        category = auction.category.title()
+        if category not in categories:
+            categories[category] = {
+                "amount": 1,
+                "auctions": [auction]
+            }
+        else:
+            categories[category]["amount"] += 1
+            categories[category]["auctions"].append(auction)
+    print(categories)
+    return render(request, "auctions/categories.html", {
+        "categories": categories,
+    })
+            
